acd44a1df26fc410d28b0e1b9392fd97ec658067
Max Kim <hwaseen@gmail.com>
2021-03-27 15:24:29 +0900

    edit server port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "maxTutorial",
       script: "./server.js",
       watch: true,
-      exec_mode: "cluster",
       env_development: {
         NODE_ENV: "development",
       },
