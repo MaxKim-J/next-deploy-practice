1e8bb7439868ea61fd380635fa8a2562559f035b
Max Kim <hwaseen@gmail.com>
2021-03-27 22:29:11 +0900

    edit content

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <title>이런식으로 헤드 커스텀이 쉽답니다~</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      요기는 디테일 페이지~! 배포가 되나요..?
+      요기는 디테일 페이지~! 배포가 되나요..?????
       <div>무중단 배포 가능한가???</div>
     </div>
   );
