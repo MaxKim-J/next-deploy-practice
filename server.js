ad98fd83676428976921ebc712d56d71c20ef6d6
Max Kim <hwaseen@gmail.com>
2021-03-27 13:19:28 +0900

    edit dependency

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const http = require("http");
-const { parse } = require("url");
+const express = require("express");
 const next = require("next");
-const fs = require("fs");
 
 const dev = process.env.NODE_ENV !== "production";
+console.log(process.env.NODE_ENV);
+
+// Next App importing
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  http
-    .createServer((req, res) => {
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
-      handle(req, res, parsedUrl);
-    })
-    .listen(80, (err) => {
-      if (err) throw err;
-      console.log("> Ready on http://localhost:80");
+app
+  .prepare()
+  .then(() => {
+    // express 서버가 모든 요청에 대해 Next에서 작성해놓은 대로 응답함
+    const server = express();
+    server.get("*", (req, res) => {
+      return handle(req, res);
     });
-});
+    server.listen(80, (err) => {
+      if (err) throw err;
+      console.log("localhost:80");
+    });
+  })
+  .catch((error) => {
+    console.error(error.stack);
+    process.exit(1);
+  });
