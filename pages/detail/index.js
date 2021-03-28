e16394b5a44609db8d0dc36e37646f0fdde4cc4e
Max Kim <hwaseen@gmail.com>
2021-03-28 12:08:38 +0900

    edit detail page

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -7,9 +7,7 @@ export default function Home() {
         <title>이런식으로 헤드 커스텀이 쉽답니다~</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      요기는 디테일 페이지~! 배포가 되나요..?????
-      <div>무중단 배포 가능한가???</div>
-      <div>으아아아 최후의 수정....이거 정말 되나.....리로드 되나...</div>
+      지금 세션중~~~
     </div>
   );
 }
